b6ff83452c966f50587a670d319a0b7e780e8050
Donya Partovi <32976948+donyaofski@users.noreply.github.com>
2025-05-26 16:35:13 +0330

    Update next.config.js

--- Next-JS-Landing-Page-Starter-Template-master/next.config.js
+++ Next-JS-Landing-Page-Starter-Template-master/next.config.js
@@ -3,15 +3,22 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer({
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   eslint: {
     dirs: ['.'],
   },
   poweredByHeader: false,
   trailingSlash: true,
-  basePath: '',
-  // The starter code load resources from `public` folder with `router.basePath` in React components.
-  // So, the source code is "basePath-ready".
-  // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
-});
+
+  // 🔽 Add these lines for GitHub Pages
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/Simplicity',
+  assetPrefix: '/Simplicity/',
+};
+
+module.exports = withBundleAnalyzer(nextConfig);
